d4f962ed0e108fa24ba9e5d297f501d4cdfb7a1b
guillaumeLeroy <guillaume.leroy34@gmail.com>
2024-09-09 17:42:40 +0200

    feur

--- app_fhir/app_fhir/views.py
+++ app_fhir/app_fhir/views.py
@@ -18,4 +18,55 @@ def call_external_api(request):
         return render(request, 'app_fhir/accueil.html', {'message': f'Other error occurred: {err}'})
 
     # Passer les données reçues à un template
-    return render(request, 'app_fhir/accueil.html', {'data': data})
+    return render(request, 'app_fhir/accueil.html', {'data': data })
+
+
+
+
+def post_patient(request):
+   data= {
+    "resourceType": "Patient",
+    "id": "66d875c65dab6800126487f6",
+    "name": [
+      {
+        "use": "official",
+        "family": "Receveur",
+        "given": [
+          "Alexandre"
+        ]
+      },
+      {
+        "use": "usual",
+        "given": [
+          "Alex"
+        ]
+      }
+    ],
+    "gender": "female",
+    "birthDate": "1974-12-25",
+    "deceasedBoolean": false
+  }
+   
+   #PATIENT CREE IMPOETANT
+   {
+  "resourceType": "Patient",
+  "id": "66df16de99cb8a001240f329",
+  "name": [
+    {
+      "use": "official",
+      "family": "Ringuet",
+      "given": [
+        "Dominique"
+      ]
+    },
+    {
+      "use": "usual",
+      "given": [
+        "Golmon"
+      ]
+    }
+  ],
+  "gender": "female",
+  "birthDate": "1980-01-20",
+  "deceasedBoolean": false
+}
