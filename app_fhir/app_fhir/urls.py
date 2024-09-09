46c8943422befe288461e3055c82e2babf5ece88
guillaumeLeroy <guillaume.leroy34@gmail.com>
2024-09-09 17:05:14 +0200

    première implémentation de la requête API

--- app_fhir/app_fhir/urls.py
+++ app_fhir/app_fhir/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('patient/', include('patient.urls') )
+    path('accueil/',views.call_external_api,name='accueil')
 ]
---
+++ app_fhir/app_fhir/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import requests
+
+
+
+def call_external_api(request):
+    # URL de l'API REST
+    url = "https://fhir.alliance4u.io/api/observation"
+    
+    # Effectuer la requête GET vers l'API
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Vérifie si la requête a échoué
+        data = response.json()  # Récupérer les données sous forme de JSON
+    except requests.exceptions.HTTPError as err:
+        return render(request, 'app_fhir/accueil.html', {'message': f'HTTP error occurred: {err}'})
+    except Exception as err:
+        return render(request, 'app_fhir/accueil.html', {'message': f'Other error occurred: {err}'})
+
+    # Passer les données reçues à un template
+    return render(request, 'app_fhir/accueil.html', {'data': data})
