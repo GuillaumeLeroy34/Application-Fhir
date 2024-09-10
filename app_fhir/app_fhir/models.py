e5a39a4940537ef6d348f0460687d32d3348855a
guillaumeLeroy <guillaume.leroy34@gmail.com>
2024-09-10 15:49:02 +0200

    fix de tout le bordel

--- app_fhir/app_fhir/models.py
+++ app_fhir/app_fhir/models.py
@@ -3,5 +3,8 @@ from django.db import models
 
 class Patient(AbstractUser):
     patientId = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    patientNumber = models.CharField(max_length=100, blank=False, null=False)
     def __str__(self):
-        return self.username
+        return self.username 
+    
+    
