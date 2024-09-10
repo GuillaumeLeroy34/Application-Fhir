9bd5875733a1bec92931148a1a62de953ac07d69
guillaumeLeroy <guillaume.leroy34@gmail.com>
2024-09-10 08:26:11 +0200

    fix url d'accueil

--- app_fhir/app_fhir/urls.py
+++ app_fhir/app_fhir/urls.py
@@ -21,5 +21,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',views.call_external_api,name='accueil')
+    path('',views.call_external_api,name='accueil')
 ]
